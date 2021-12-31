4c126ef64b6a568069902a434b4e43e2102985fe
TmLololol <tmtanky@gmail.com>
2021-12-31 15:35:24 +0800

    configured sass/scss

---
+++ src/components/Searchbar/Seachbar.svelte
@@ -0,0 +1,8 @@
+<main>
+    <input type="search" name="searchbox">
+    <button> Search </button>
+</main>
+
+<style lang="scss">
+
+</style>
---
+++ src/routes/Home/Home.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import Searchbar from "../../components/Searchbar/Seachbar.svelte";
+</script>
+
+<main>
+    <Searchbar />
+</main>
