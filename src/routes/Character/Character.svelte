f1c18b131b78d76169349dd2295f1e1d3e114aa6
TmLololol <tmtanky@gmail.com>
2022-01-21 23:27:19 +0800

    configured the API call for the page

--- src/routes/Character/Character.svelte
+++ src/routes/Character/Character.svelte
@@ -1,8 +1,30 @@
 <script lang="ts">
-    import { onMount } from 'svelte'
+	import { onMount } from 'svelte'
+	import type { DataWrapper } from 'src/ts/interfaces/DataWrapper'
+	import type { Character } from 'src/ts/interfaces/Character'
 
+	const path = window.location.pathname
+	const id = path.substring(11, path.length)
+
+	let character = {} as Character
+	let loading = true
+
+	onMount(async () => {
+		const data = await fetch(
+			`https://gateway.marvel.com/v1/public/characters/${id}?ts=1&apikey=${
+				import.meta.env.VITE_API_KEY
+			}&hash=${import.meta.env.VITE_HASH}`
+		)
+		const res = (await data.json()) as DataWrapper
+		character = res.data.results[0]
+		loading = false
+	})
 </script>
 
 <main>
-    <h1> Character Page </h1>
+	{#if loading}
+		<p>Loading...</p>
+	{:else}
+		<h1>{character.name}</h1>
+	{/if}
 </main>
